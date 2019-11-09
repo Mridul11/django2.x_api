058f29d06fa3b8fa7e897b8a91349dd86bb48b2a
mridul <mridulmishra2991@gmail.com>
2019-11-10 00:58:40 +0530

    added class based views with mixins

--- updates/views.py
+++ updates/views.py
@@ -1,3 +1,37 @@
+import json 
+from django.http import JsonResponse, HttpResponse 
 from django.shortcuts import render
+from django.views.generic import View   
+from rest_api.mixins import JsonResponseMixin
+from .models import Update 
 
-# Create your views here.
+# def detail_view(request, response):
+#     return render() 
+
+def json_example_view(request):
+    data = {  
+        "count" : 11,
+        "content" : "some new content"
+    }
+    # json_data = json.dumps(data)
+    return JsonResponse(data)
+    # return HttpResponse(json_data)
+
+class JsonCBV(View):
+    def get(self, request, *args, **kwargs):
+        data = {  
+            "count" : 11,
+            "content" : "some new content"
+            }
+        # json_data = json.dumps(data)
+        return JsonResponse(data)
+
+
+class JsonCBV2(JsonResponseMixin, View):
+    def get(self, request, *args, **kwargs):
+        data = {
+            "count" : 11,
+            "content" : "some new content"
+        }
+        return self.render_to_json_response(data)
+
