ef86c4843808d3e588df1ccb7c0aa4eea1ef7a2d
mridul <mridulmishra2991@gmail.com>
2019-11-10 01:23:41 +0530

    added serialization from django-library's serialize function to view file!

--- rest_api/urls.py
+++ rest_api/urls.py
@@ -1,13 +1,13 @@
 
 from django.contrib import admin
 from django.urls import path
-from updates.views import json_example_view , JsonCBV, JsonCBV2 
+from updates.views import json_example_view , JsonCBV, JsonCBV2 , SerializeListView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', json_example_view ),
     path('cbv/', JsonCBV.as_view() ),
-    path('cbv2/', JsonCBV2.as_view() )
-
+    path('cbv2/', JsonCBV2.as_view() ),
+    path('serializelistview/', SerializeListView.as_view() )
 
 ]
--- updates/views.py
+++ updates/views.py
@@ -2,6 +2,7 @@ import json
 from django.http import JsonResponse, HttpResponse 
 from django.shortcuts import render
 from django.views.generic import View   
+from django.core.serializers import serialize 
 from rest_api.mixins import JsonResponseMixin
 from .models import Update 
 
@@ -34,4 +35,30 @@ class JsonCBV2(JsonResponseMixin, View):
             "content" : "some new content"
         }
         return self.render_to_json_response(data)
+
+
+class SerializeDetailView(View):
+    def get(self, request, *args, **kwargs):
+        obj = Update.objects.find(id=1)
+        data = {
+            "user" : obj.user.name ,
+            "content" : obj.user.content 
+        }
+
+        json_data = json.dumps(data)
+        return HttpResponse(json_data, content_typ='application/json')
+
+
+class SerializeListView(View):
+    def get(self, request, *args, **kwargs):
+        qs = Update.objects.all()
+        data = serialize("json", qs, fields=('user', 'content'))
+        print(data)
+        json_data = data
+        # data = {
+        #     "user" : obj.user.name ,
+        #     "content" : obj.user.content 
+        # }
+        # json_data = json.dumps(data)
+        return HttpResponse(json_data, content_type='application/json')
 
